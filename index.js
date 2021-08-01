07005a7b6963258e233d6758e3a6772e8a84bbb3
Chathuranga <nchathu2014@gmail.com>
2021-08-02 00:50:36 +0530

    Add rental API

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const health = require('./routes/health');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use('/health', health);
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening to port ${port}`));
