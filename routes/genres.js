1d513b45dcb41177d666cf8bd106e261e32d4ac5
Chathuranga <nchathu2014@gmail.com>
2021-08-08 08:52:12 +0530

    Add rolebase authorization

--- routes/genres.js
+++ routes/genres.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const admin = require('../middleware/admin');
 const router = express.Router();
 const auth = require('./../middleware/auth');
 
@@ -42,7 +43,7 @@ router.put('/:id', auth, async (req, res) => {
 });
 
 //DELETE
-router.delete('/:id', auth, async (req, res) => {
+router.delete('/:id', [auth, admin], async (req, res) => {
     const genre = await Genre.findByIdAndRemove(req.params.id)
     if (!genre) return res.status(404).send('No genre find for this Id');
     res.send(genre)
